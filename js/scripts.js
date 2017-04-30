f0caf9ea010d65517a3a79a40722ee25fb5494f7
Rhavila Medeiros <rhavila_pmn@hotmail.com>
2017-04-30 16:12:15 -0300

    Hamburguer Menu

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  //hamburguer menu toggle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburguer').toggleClass('is-open');
+  });
+
+});
