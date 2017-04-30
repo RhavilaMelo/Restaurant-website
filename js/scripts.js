66c1502e2504b33d731381d7000451551f01e6eb
Rhavila Medeiros <rhavila_pmn@hotmail.com>
2017-04-30 19:50:21 -0300

    Smooth Scroll

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
   });
 
 });
+
+
+$('.usmain a[href^="#"]').click(function(e){
+  $('.main-nav').toggleClass('is-open');
+  $('.hamburguer').toggleClass('is-open');
+});
+
+//Smooth scroll
+
+$('.usmain a[href^="#"]').click(function(e){
+
+  if ($(window).width() > 725){ // if the screen is bigger than 725 it'll do do this code snippet
+      e.preventDefault();
+        var id = $(this).attr('href'),
+            menuHeight = $('.usmain').innerHeight(),
+            targetOffset = $(id).offset().top;
+      $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+      }, 500);
+  }
+});
